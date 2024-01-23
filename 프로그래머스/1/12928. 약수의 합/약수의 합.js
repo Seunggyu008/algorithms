647d146b2eb7501103794e715a87d95ee566a2d4
Jung Seunggyu <56462409+Seunggyu008@users.noreply.github.com>
2024-01-23 19:06:17 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,14 +1,12 @@
 function solution(n) {
     let arr = [];
     for(let i = 0; i <= n; i++) {
-            arr.push(n / i);    
+            if(n % i === 0) {
+                arr.push(i);
+            }  
     } 
-    
-    const filteredArr = arr.filter((num) => Number.isInteger(num));
-    console.log(filteredArr);
-    
-    return filteredArr.reduce((a, c) => a + c, 0);
+    return arr.reduce((a, c) => a + c, 0);
 }
 
-solution(5);
+solution(12);
 
